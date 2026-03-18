941e800c0be3b4b7a7c62b0123558fb2b9d4225e
Benkaddour Redouane <redou.benka1@gmail.com>
2026-03-18 13:13:53 +0100

    Relation ManyToMany between User & Representation

--- src/main/java/com/example/springboot_app/model/Representation.java
+++ src/main/java/com/example/springboot_app/model/Representation.java
@@ -10,9 +10,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -39,6 +43,14 @@ public class Representation {
 	@ManyToOne
 	@JoinColumn(name="location_id", nullable=true)
 	private Location location;
+        
+        @ManyToMany
+	@JoinTable(
+		  name = "reservations", 
+		  joinColumns = @JoinColumn(name = "representation_id"), 
+		  inverseJoinColumns = @JoinColumn(name = "user_id"))
+	private List<User> users = new ArrayList<>();
+
 
 	public Representation() { }
 	
@@ -75,6 +87,29 @@ public class Representation {
 	public Long getId() {
 		return id;
 	}
+        
+        public List<User> getUsers() {
+		return users;
+	}
+
+	public Representation addUser(User user) {
+		if(!this.users.contains(user)) {
+			this.users.add(user);
+			user.addRepresentation(this);
+		}
+		
+		return this;
+	}
+	
+	public Representation removeUser(User user) {
+		if(this.users.contains(user)) {
+			this.users.remove(user);
+			user.getRepresentations().remove(this);
+		}
+		
+		return this;
+	}
+
 
 	@Override
 	public String toString() {
